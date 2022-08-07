60be25e551e1735fb34c1d112fc601a74b164a49
Kirill Velikanov <velik.kir@gmail.com>
2022-08-07 21:20:46 +0300

    Тёмная тема обновлена

--- lib/src/misc/theme/dark_theme.dart
+++ lib/src/misc/theme/dark_theme.dart
@@ -7,15 +7,37 @@ final darkTheme = ThemeData(
   backgroundColor: _backPrimary,
   scaffoldBackgroundColor: _backPrimary,
   appBarTheme: const AppBarTheme(backgroundColor: _backPrimary),
+  textTheme: const TextTheme(
+    titleLarge:
+        TextStyle(fontSize: 32, height: 38 / 32, fontWeight: FontWeight.w500),
+    titleMedium:
+        TextStyle(fontSize: 20, height: 32 / 20, fontWeight: FontWeight.w500),
+    bodyMedium:
+        TextStyle(fontSize: 16, height: 20 / 16, fontWeight: FontWeight.w400),
+    titleSmall:
+        TextStyle(fontSize: 14, height: 20 / 14, fontWeight: FontWeight.w400),
+  ),
+  textButtonTheme: TextButtonThemeData(
+    style: TextButton.styleFrom(
+      primary: _blue,
+      textStyle: const TextStyle(
+        fontSize: 14,
+        height: 24 / 14,
+        fontWeight: FontWeight.w500,
+      ),
+    ),
+  ),
   colorScheme: ColorScheme.fromSeed(
     seedColor: _blue,
     background: _backPrimary,
     onPrimary: _labelPrimary,
     onSecondary: _labelSecondary,
+    onTertiary: _labelTertiary,
     surface: _backsecondary,
   ),
   iconTheme: const IconThemeData(color: _blue),
   disabledColor: _labelDisable,
+  hintColor: _labelTertiary,
   floatingActionButtonTheme: const FloatingActionButtonThemeData(
     backgroundColor: _blue,
   ),
