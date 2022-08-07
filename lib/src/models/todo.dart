ff6d73920f917b17bf9f7c0d6955860d809a1e46
Kirill Velikanov <velik.kir@gmail.com>
2022-08-07 19:55:43 +0300

    feat: Абстракция локального хранилища и реализация
    Реализация с помощью hive

--- lib/src/models/todo.dart
+++ lib/src/models/todo.dart
@@ -2,22 +2,25 @@
 
 import 'package:flutter/foundation.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:hive/hive.dart';
 
 part 'todo.freezed.dart';
 part 'todo.g.dart';
 
 @Freezed()
 class Todo with _$Todo {
+  @HiveType(typeId: 0, adapterName: "TodoAdapter")
   const factory Todo({
-    String? id,
-    required String text,
-    @Default(Importance.low) Importance importance,
+    @HiveField(0) String? id,
+    @HiveField(1) required String text,
+    @Default(Importance.low) @HiveField(2) Importance importance,
     @JsonKey(fromJson: _dateTimefromJson, toJson: _dateTimeToJson)
+    @HiveField(3)
         DateTime? deadline,
-    @Default(false) bool done,
-    @JsonKey(name: 'created_at') int? createdAt,
-    @JsonKey(name: 'changed_at') int? changedAt,
-    @JsonKey(name: 'last_updated_by') String? lastUpdatedBy,
+    @Default(false) @HiveField(4) bool done,
+    @JsonKey(name: 'created_at') @HiveField(5) int? createdAt,
+    @JsonKey(name: 'changed_at') @HiveField(6) int? changedAt,
+    @JsonKey(name: 'last_updated_by') @HiveField(7) String? lastUpdatedBy,
   }) = _Todo;
 
   factory Todo.fromJson(Map<String, Object?> json) => _$TodoFromJson(json);
@@ -29,11 +32,20 @@ DateTime? _dateTimefromJson(int? int) =>
     int == null ? null : DateTime.fromMillisecondsSinceEpoch(int);
 int? _dateTimeToJson(DateTime? time) => time?.millisecondsSinceEpoch;
 
+@HiveType(typeId: 2, adapterName: "ImportanceAdapter")
 enum Importance {
   @JsonValue("low")
-  low,
+  @HiveField(0)
+  low("low"),
+
+  @HiveField(1)
   @JsonValue("basic")
-  basic,
-  @JsonValue("inportant")
-  important;
+  basic("basic"),
+
+  @HiveField(2)
+  @JsonValue("important")
+  important("important");
+
+  final String importance;
+  const Importance(this.importance);
 }
---
+++ lib/src/services/local_service/local_service.dart
@@ -0,0 +1,2 @@
+export 'hive.dart';
+export 'abstract_local_service.dart';
